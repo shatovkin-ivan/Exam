65a45502f8e9772ae2e3a212b5533caa2b25546f
IvanSh <shatowkin.ivan@yandex.ru>
2020-09-04 13:51:07 +0300

    9

--- script/script.js
+++ script/script.js
@@ -3,32 +3,59 @@ window.addEventListener('DOMContentLoaded', () => {
    // появляющаяся кнопка
 
    window.addEventListener('scroll', function () {
-      const header = document.querySelector('.arrow__top'),
+      const arrow = document.querySelector('.arrow__top'),
          banner = document.querySelector('.luxury'),
-         height = banner.offsetHeight;
+         header = document.querySelector('.header'),
+         height = banner.offsetHeight + header.offsetHeight;
 
       function scroll() {
          if (window.pageYOffset > height) {
-            header.classList.add('arrow__top_active');
+            arrow.classList.add('arrow__top_active');
          }
          else {
-            header.classList.remove('arrow__top_active');
+            arrow.classList.remove('arrow__top_active');
          }
       }
       scroll();
    })
-})
 
-$(document).ready(function () {
+   //    // выпадающие окна
 
-   // скролл
+   //    const links = document.querySelectorAll('.arrow'),
+   //       dropdown = document.querySelectorAll('.filter__dropdown');
 
-   $('a.anchor').click(function (e) {
-      if ($(window).width() > 768) {
-         e.preventDefault();
-         var elClick = $(this).attr('href');
-         var dest = $(elClick).offset().top;
-         $('html:not(:animated),body:not(:animated)').animate({ scrollTop: dest }, 1000);
-      }
-   });
+   //    links.forEach(item => {
+   //       item.addEventListener('click', selectElement)
+   //    });
+   //    function selectElement() {
+   //       dropdown.forEach(e => {
+   //          e.preventDefault();
+   //          this.classList.toggle('filter__dropdown_active');
+   //          this.classList.toggle('arrow_active');
+   //       })
+   //    }
+   // })
+
+   $(document).ready(function () {
+
+      $('.filter__descr').on('click', function (event) {
+         event.preventDefault();
+         $(this).parents('.filter__item').find('.filter__dropdown').slideToggle();
+         $(this).parents('.filter__item').find('.arrow').toggleClass('arrow_active');
+         if ($(window).width() < 1280) {
+            $(this).parents('.filter__item').find('.arrow').toggleClass('arrow_active');
+         }
+      });
+
+      // скролл
+
+      $('a.anchor').click(function (e) {
+         if ($(window).width() > 768) {
+            e.preventDefault();
+            var elClick = $(this).attr('href');
+            var dest = $(elClick).offset().top;
+            $('html:not(:animated),body:not(:animated)').animate({ scrollTop: dest }, 1000);
+         }
+      });
+   })
 })
